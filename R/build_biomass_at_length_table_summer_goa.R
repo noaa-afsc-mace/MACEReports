d9c1b5ac01c330a066cfeac9f682005e9e5bb68e
mike.levine <mike.levine@noaa.gov>
2022-10-20 16:28:36 -0700

    added goa summer biomass-at-length table

--- R/build_biomass_at_length_table_summer_goa.R
+++ R/build_biomass_at_length_table_summer_goa.R
@@ -1,6 +1,6 @@
-#' @title build_numbers_at_length_table_summer_goa
+#' @title build_biomass_at_length_table_summer_goa
 #'
-#' @description Build the pollock numbers-at-length table used in summer GOA reports. This will report all lengths
+#' @description Build the pollock biomass-at-length table used in summer GOA reports. This will report all lengths
 #' from 10-70 cm, and will do so for each Report Number (i.e. survey region) that was defined for the analysis.
 #' If you have an especially small (<10 cm) or large (>70 cm) length class, the table will report these sizes as opposed
 #' to the nomimal 10 cm- 70 cm range.
@@ -8,7 +8,7 @@
 #' in the cruise report markdown process using the function \code{get_biomass_and_nums_data_function.R}; this provides all
 #' the data that is then used to create a 'current survey only/pollock only' dataframe named
 #' \code{current_survey_pollock_biomass_nums}. This dataframe is the easiest input to
-#'  \code{build_numbers_at_length_table_summer_goa} function.
+#'  \code{build_numbers_at_length_table_summer_goa} function
 #'
 #'  @return A list with two items: item 1 is the Flextable table object, item 2 is the table caption.
 #'
@@ -17,15 +17,15 @@
 #' @examples
 #' \dontrun{
 #' #build the table and caption
-#' nums_table_list =
-#' build_numbers_at_length_table_summer_goa(biomass_nums_data = current_survey_pollock_biomass_nums)
+#' biomass_table_list =
+#' build_biomass_at_length_table_summer_goa(biomass_nums_data = current_survey_pollock_biomass_nums)
 #'
 #' #pull out table and caption from the list
-#' nums_table =  nums_table_list[[1]]
-#' nums_caption = nums_table_list[[2]]
+#' biomass_table =  biomass_table_list[[1]]
+#' biomass_caption = biomass_table_list[[2]]
 #' }
 #' @export
-build_numbers_at_length_table_summer_goa = function(biomass_nums_data){
+build_biomass_at_length_table_summer_goa = function(biomass_nums_data){
 
   #data checks:
 
@@ -52,35 +52,35 @@ build_numbers_at_length_table_summer_goa = function(biomass_nums_data){
                 paste(shQuote(unique(biomass_nums_data$SURVEY)), collapse=", "), '\n'))
   }
 
-
   #######
-  #step 1: sum nums vertically by interval, length bin; report as numbers (million)
-  biomass_nums_summary = biomass_nums_data%>%
+  #step 1: sum biomass vertically by interval, length bin; report as biomass (t)
+  biomass_summary = biomass_nums_data%>%
     dplyr::group_by(.data$report_number, .data$region, .data$LENGTH)%>%
-    dplyr::summarize(num_million = sum(.data$NUMBERS)/1e6)%>%
+    dplyr::summarize(biomass_t = sum(.data$BIOMASS)/1e3)%>%
     dplyr::arrange(.data$report_number, .data$LENGTH)
 
   #add a totals column as well
-  table_totals = biomass_nums_summary%>%
+  table_totals = biomass_summary%>%
     dplyr::group_by(.data$LENGTH)%>%
-    dplyr::summarize(Total = sum(.data$num_million))
+    dplyr::summarize(Total = sum(.data$biomass_t))
 
   #####
   #step 2: get vectors of every possible length in the survey areas:
 
-  #we report all lengths from 10-70 cm; it's unlikely that any given survey contains all these, but we want to report a blank in these cases
+  #we report all lengths from 10-70 cm; it's unlikely that any given survey contains all these,
+  #but we want to report a blank in these cases
 
   #build a dataframe that's simply each possible length per region
   get_all_possible_lengths_for_region = function(region_name){
 
     #set the minimum length at 10 cm UNLESS there are smaller fish in the survey
-    min_report_length = ifelse(min(biomass_nums_summary$LENGTH) < 10,
-                               min(biomass_nums_summary$LENGTH),
+    min_report_length = ifelse(min(biomass_summary$LENGTH) < 10,
+                               min(biomass_summary$LENGTH),
                                10)
 
     #set the maximum length as 70 cm UNLESS there are larger fish in the survey
-    max_report_length = ifelse(max(biomass_nums_summary$LENGTH > 70),
-                               max(biomass_nums_summary$LENGTH),
+    max_report_length = ifelse(max(biomass_summary$LENGTH > 70),
+                               max(biomass_summary$LENGTH),
                                70)
 
     #build a dataframe that has the length vector and report name
@@ -94,26 +94,26 @@ build_numbers_at_length_table_summer_goa = function(biomass_nums_data){
   }
 
   #apply function to get dataframe of lengths
-  report_lengths_for_regions = purrr::map_dfr(unique(biomass_nums_summary$region), get_all_possible_lengths_for_region)
+  report_lengths_for_regions = purrr::map_dfr(unique(biomass_summary$region), get_all_possible_lengths_for_region)
 
   #and add the blank rows where no fish were observed
-  biomass_nums_summary = dplyr::left_join(report_lengths_for_regions, biomass_nums_summary, by = c('LENGTH', 'region'))
+  biomass_summary = dplyr::left_join(report_lengths_for_regions, biomass_summary, by = c('LENGTH', 'region'))
 
   #######
   #3. make the table
 
   #for presentation, we just want numbers (millions) for each length bin (10-70 cm and geographic area)
-  nums_for_presentation = biomass_nums_summary%>%
+  biomass_for_presentation = biomass_summary%>%
     #remove the report_number column- it was only needed to order things by report number
-    dplyr::select(.data$LENGTH, .data$region, .data$num_million)%>%
+    dplyr::select(.data$LENGTH, .data$region, .data$biomass_t)%>%
     #also, go from 'long' format (where every region lives in it's own)
-    tidyr::pivot_wider(id_cols = .data$LENGTH, names_from = .data$region, values_from = .data$num_million)
+    tidyr::pivot_wider(id_cols = .data$LENGTH, names_from = .data$region, values_from = .data$biomass_t)
 
   #join totals to the 'wide' dataframe
-  nums_for_presentation = dplyr::left_join(nums_for_presentation, table_totals, by = c('LENGTH'))
+  biomass_for_presentation = dplyr::left_join(biomass_for_presentation, table_totals, by = c('LENGTH'))
 
   #rename LENGTH to a nicer title for plotting
-  nums_for_presentation = nums_for_presentation%>%
+  biomass_for_presentation = biomass_for_presentation%>%
     dplyr::rename('Length' = 'LENGTH')
 
   #calculate a row of sums for the bottom of table. Note na.rm = TRUE (because there are lots
@@ -122,25 +122,25 @@ build_numbers_at_length_table_summer_goa = function(biomass_nums_data){
   #this will sum up every column in the survey, except for the first (which is the Length column)
   #we'll just add 'Total' here  to represent the length column, and add up the rest as their own column
   # do this in a loop as the number of survey areas (i.e. columns in table) can differ annually
+
   col_sums= c('Total')
-  for (i in 2:ncol(nums_for_presentation)){
+  for (i in 2:ncol(biomass_for_presentation)){
 
     #get the column summed; format for table
-    col_sum_formatted = format(sum(nums_for_presentation[,i], na.rm = TRUE),
-                               big.mark = ",",  na_str = '-', nsmall = 2, digits = 2, scientific = FALSE)
+    col_sum_formatted = format(sum(biomass_for_presentation[,i], na.rm = TRUE), big.mark = ",",  na_str = '-', nsmall = 2, digits = 2, scientific = FALSE)
 
     #add it to the collection of formatted sums
     col_sums = cbind(col_sums, col_sum_formatted)
 
   }
 
-  #define a border to add above and below caption and at bottom of table
+  #define a border to use above and below caption and at bottom of table
   table_border = officer::fp_border(color="black", width = 0.75)
 
-  #make the nums-at-length table into a flextable
-  nums_length_table = flextable::flextable(nums_for_presentation)%>%
+  #make the biomass-at-length summary into a flextable
+  biomass_length_table = flextable::flextable(biomass_for_presentation)%>%
     #append totals as a footer row
-    flextable::add_footer_row(top = FALSE, values = col_sums, colwidths = rep(1,length(nums_for_presentation)))%>%
+    flextable::add_footer_row(top = FALSE, values = col_sums, colwidths = rep(1,length(biomass_for_presentation)))%>%
     #get rid of padding around cells
     flextable::padding(padding = 0, part = "all")%>%
     #align text: center justify everything
@@ -148,7 +148,6 @@ build_numbers_at_length_table_summer_goa = function(biomass_nums_data){
     #add horizontal border on top and bottom of table
     flextable::hline_top(part="all", border = table_border)%>%
     flextable::hline_bottom(part="all", border = table_border)
-
 
   #create a function to format catch weights and percents and numbers percents conditionally:
   #if values = NA, label as '-'
@@ -161,22 +160,21 @@ build_numbers_at_length_table_summer_goa = function(biomass_nums_data){
   #to format all the columns that could exist (i.e. different years will have different numbers of reporting columns),
   #we need a named list of column names and the function to apply to said column;
   #this needs to skip the first column too (no need to format 'Length')
-  funs_list =  stats::setNames(rep(list(pretty_nums_function),
-                                   length(names(nums_for_presentation))-1),
-                               names(nums_for_presentation)[-1])
+  funs_list =  stats::setNames(rep(list(pretty_nums_function), length(names(biomass_for_presentation))-1),
+                               names(biomass_for_presentation)[-1])
 
   #apply this function to the specfied table columns
-  nums_length_table = flextable::set_formatter(nums_length_table, values = funs_list)%>%
+  biomass_length_table = flextable::set_formatter(biomass_length_table, values = funs_list)%>%
     #set the font and font size
     flextable::font(fontname = 'times', part = 'all')%>%
     flextable::fontsize(size = 10, part = 'header')%>%
     flextable::fontsize(size = 10, part = 'body')
 
   #build the caption
-  cap_text = paste0('Pollock numbers-at-length estimates (millions of fish) by survey region during the ',
+  cap_text = paste0('Pollock biomass-at-length estimates (metric tons) by survey region during the ',
                     unique(biomass_nums_data$year), ' GOA survey.' )
 
   #return table and caption
-  return(list(nums_length_table, cap_text))
+  return(list(biomass_length_table, cap_text))
 
 }
